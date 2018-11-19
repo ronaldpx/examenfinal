85ceff2722dd59d4673427276f7152592d13fd99
ronaldpx <rconio.q@gmail.com>
2018-11-19 12:04:44 -0600

    prueba 3

--- pensum/views.py
+++ pensum/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import GradoForm
-from pensum.models import Grado, Materia
+from pensum.models import Grado, Asignacion
 # Create your views here.
 
 def grado_nueva(request):
     if request.method == "POST":
         formulario = GradoForm(request.POST)
         if formulario.is_valid():
-            pelicula = Grado.objects.create(nombre=formulario.cleaned_data['nombre'], seccion = formulario.cleaned_data['seccion'])
+            grado = Grado.objects.create(nombre=formulario.cleaned_data['nombre'], seccion = formulario.cleaned_data['seccion'])
             for materia_id in request.POST.getlist('materias'):
                 asignacion = Asignacion(materia_id=materia_id, grado_id = grado.id)
                 asignacion.save()
